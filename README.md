873e62ad4beab26092eccf901035b245bf943919
Jaime Cartes <jcartesg@live.com>
2025-01-29 18:36:37 -0300

    Actualiza Readme y agraga código

--- README.md
+++ README.md
@@ -9,4 +9,17 @@ Luego de esto, la app escogerá al azar y entregará un único nombre de todos l
 Paso 1: Se declara una variable de tipo array (arreglo o lista), que almacenará los nombres de los amigos y amigas ingresados en la app.
     let nombres = [];
 
-Paso 2: Se crea la función "agregarAmigo"
+Paso 2: Se crea la función agregarAmigo()
+Esta función se encarga de comprobar que los nombres sean válidos y luego los incorpora al array "nombres"
+
+Dentro del bloque de la función agregarAmigo() se encuentran estas otras funciones:
+
+a.- La función limpiarcaja() se encarga de limpiar la caja "Escribe un nombre" cada vez que se ingresa un nuevo amigo.
+
+b.- La función agregarAListaUl() se encarga de crear un nuevo elemento de lista (<li>) y agregarlo a la lista 
+desordenada (<ul>) con el ID "listaAmigos".
+
+Cada vez que se llama a la función agregarAListaUl() con un nuevo elemento, se crea un nuevo <li> y se agrega al final de la lista.
+
+Esto hará que los elementos se muestren uno debajo del otro a medida 
+que se van agregando.
--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 let nombres = [];
 
 
-//<button class="button-add" onclick="agregarAmigo()">Añadir</button>
-//la función para el botón "añadir" se llama "agregarAmigo()"
-function agregarAmigo() { //"agregarAmigo" está definido en el html
+//FUNCION PARA el botón "agregarAmigo"
+// Agraga amigos a la array "nombres"
+function agregarAmigo() { 
     let nuevoAmigo = document.getElementById('amigo').value;
+    if (nuevoAmigo == ""){
+        alert('Por favor ingrese un nombre válido');
+    } else {
     nombres.push(nuevoAmigo);
     limpiarCaja();
     agregarAListaUl(nuevoAmigo);
@@ -13,18 +16,27 @@ console.log(nombres.length);
 console.log(nombres[0]);
 return;
 }
+}
 
 function limpiarCaja(){
     let valorCaja = document.querySelector('#amigo');
     valorCaja.value = '';
 }
-//<button class="button-draw" onclick="sortearAmigo()" 
-// aria-label="Sortear amigo secreto">
-//LA FUNCIÓN PARA EL BOTÓN "SORTEAR AMIGO" SE LLAMA "sortearAmigo()"
+
+// FUNCIÓN PARA EL BOTÓN "sortearAmigo"
 function  sortearAmigo() { //"sortearAmigo" está definido en el html
     alert('Hicimos click en el botón "Sortear amigo"');
+    vaciarListaUl('#listaAmigos');
     return;
 }
+
+//Función para vaciar cualquier lista <ul>
+function vaciarListaUl(elemento){
+// Seleccionar el elemento <ul>
+let listaUl = document.querySelector(elemento);
+listaUl.innerHTML = ''; //Vaciar el contenido del elemento <ul>
+}
+
 
 //FUNCIÓN PARA AGREGAR UN NÚMERO ALEATORIO
 /*
@@ -39,38 +51,12 @@ alert(`El número aleatorio es ${numeroAleatorio}`);
 //<ul id="myList"></ul> ("myList" = "listaAmigos")
 
 // Función para agregar un nuevo elemento a la "lista ul"
-function agregarAListaUl(item) { //addToList
-// Crear un nuevo elemento de lista (<li>)
+function agregarAListaUl(item) { 
+// Crear un nuevo elemento de lista (<li>) 
 let nuevoNombre = document.createElement('li');
 nuevoNombre.textContent = item;
-
-// Obtener la lista ul y agregar el nuevo elemento
-let list = document.getElementById('listaAmigos');
-list.appendChild(nuevoNombre);
+// Obtener la lista <ul> y agregar el nuevo elemento
+let listaUl = document.getElementById('listaAmigos');
+listaUl.appendChild(nuevoNombre); //agrega el nuevo nombre la lista <ul>
 }
 
-// Agregar algunos elementos a la lista
-//agregarALista('Elemento 1');
-//agregarALista('Elemento 2');
-//agregarALista('Elemento 3');
-
-/*
-En este ejemplo, tenemos una función agregarALista() que se encarga de 
-crear un nuevo elemento de lista (<li>) y agregarlo a la lista 
-desordenada (<ul>) con el ID "listaAmigos".
-
-Cada vez que se llama a la función addToList() con un nuevo elemento, 
-se crea un nuevo <li> y se agrega al final de la lista.
-
-Esto hará que los elementos se muestren uno debajo del otro a medida 
-que se van agregando.
-
-Puedes llamar a la función addToList() tantas veces como quieras para
- agregar más elementos a la lista. Por ejemplo:
-
-addToList('Elemento 4');
-addToList('Elemento 5');
-addToList('Elemento 6');
-
-Y la lista se irá actualizando con los nuevos elementos.
-*/
