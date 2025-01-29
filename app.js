bd9320f2476b083b3887c1c0e0cd151517dbea79
Jaime Cartes <jcartesg@live.com>
2025-01-29 00:10:42 -0300

    actualiza app,js

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades 
-// en lógica de programación. Aquí deberás desarrollar la lógica para 
-// resolver el problema.
-alert('Hola mundo');
+let nombres = [];
+console.log(nombres);
+
+
+//<button class="button-add" onclick="agregarAmigo()">Añadir</button>
+//la función para el botón "añadir" se llama "agregarAmigo()"
+function agregarAmigo() { //"agregarAmigo" está definido en el html
+    let nuevoAmigo = document.getElementById('amigo').value;
+    nombres.push(nuevoAmigo);
+console.log(nombres);
+console.log(nombres.length);
+console.log(nombres[2]);
+return;
+}
+
+function asignarTextoElemento(elemento,texto){
+    let elementoHtml = document.querySelector(elemento);
+    elementoHtml.innerHTML = texto;
+    return;
+}
+asignarTextoElemento('u1',nuevoAmigo);
+
+//<button class="button-draw" onclick="sortearAmigo()" 
+// aria-label="Sortear amigo secreto">
+//LA FUNCIÓN PARA EL BOTÓN "SORTEAR AMIGO" SE LLAMA "sortearAmigo()"
+function  sortearAmigo() { //"sortearAmigo" está definido en el html
+    alert('Hicimos click en el botón "Sortear amigo"');
+    return;
+}
+
+//FUNCIÓN PARA AGREGAR UN NÚMERO ALEATORIO
+/*
+function generaNumeroAleatorio() {
+    return Math.floor(Math.random()*10)+1;
+}
+let numeroAleatorio = generaNumeroAleatorio();
+alert(`El número aleatorio es ${numeroAleatorio}`);
+*/
